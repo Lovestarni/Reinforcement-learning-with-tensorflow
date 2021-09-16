3ee218b1217a806fc6388544111f5245fefec8a6
“skywalker” <lovestarsni@gmail.com>
2021-09-16 20:32:38 +0800

    + learning policy gradients

--- contents/7_Policy_gradient_softmax/RL_brain_torch.py
+++ contents/7_Policy_gradient_softmax/RL_brain_torch.py
@@ -8,12 +8,14 @@ import numpy as np
 # import tensorflow as tf
 import torch
 from tensorboardX import SummaryWriter
+from torch.distributions import Categorical
 
 # reproducible
 torch.manual_seed(1)
 
 
 class Net(torch.nn.Module):
+    # 离散空间采用policy gradient优化策略
     def __init__(self, configs):
         super(Net, self).__init__()
         self.fc1 = torch.nn.Linear(configs['input_size'], configs[
@@ -45,26 +47,39 @@ class PolicyGradient:
         self.lr = learning_rate
         self.gamma = reward_decay
 
-        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+        self.ep_obs, self.ep_as, self.ep_rs, self.ep_as_prob = [], [], [], []
 
-        self.net = Net({
+        self.pollicy_net = Net({
             'input_size': self.n_features,
             'hidden_size': 10,
             'output_size': self.n_actions
         })
 
+        self.optimizer = torch.optim.Adam(self.pollicy_net.parameters(),
+                                          lr=self.lr)
+        # 不进行reduction操作，方便后面和v_t相乘
+        self.loss_func = torch.nn.NLLLoss(reduction='none')
+
         if output_graph:
             # $ tensorboard --logdir=logs
             # http://0.0.0.0:6006/
-            # tf.train.SummaryWriter soon be deprecated, use following
+            # tf.train.SummaryWriter sowaka
             self.writer = SummaryWriter("logs/")
-            self.writer.add_graph(self.net, torch.ones((2, n_features)))
+            self.writer.add_graph(self.pollicy_net, torch.ones((2, n_features)))
             self.writer.add_scalar('a', self.n_actions)
 
         # self.sess.run(tf.global_variables_initializer())
 
     def choose_action(self, observation):
-        pass
+        # 推断的时候是一个一个输入的，所以这里要加一个唯独
+        observation = torch.unsqueeze(torch.FloatTensor(observation), 0)
+        pro_weights = self.pollicy_net(observation)
+        # 概率分布
+        self.ep_as_prob.append(pro_weights[0].data)
+        m = Categorical(pro_weights)
+        # 变为类别分布，然后采样
+        action = m.sample()
+        return action.item()
         # return action
 
     def store_transition(self, s, a, r):
@@ -73,14 +88,28 @@ class PolicyGradient:
         self.ep_rs.append(r)
 
     def learn(self):
-        pass
+        discounted_ep_rs_norm = torch.FloatTensor(
+            self._discount_and_norm_rewards())
+
         # return discounted_ep_rs_norm
+        # 这里要梯度上升，最大化似然
+        loss = torch.mean(-self.loss_func(torch.stack(self.ep_as_prob),
+                                          torch.LongTensor(
+                                              self.ep_as)) *
+                          discounted_ep_rs_norm)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        # train on episode
 
     def _discount_and_norm_rewards(self):
         # discount episode rewards
+        # 因为每次是收集的一个回合的数据，要重新处理下reward，以便满足折扣银子的要求
         discounted_ep_rs = np.zeros_like(self.ep_rs)
         running_add = 0
         for t in reversed(range(0, len(self.ep_rs))):
+            # 越近的reward的重要程度越高
             running_add = running_add * self.gamma + self.ep_rs[t]
             discounted_ep_rs[t] = running_add
 
